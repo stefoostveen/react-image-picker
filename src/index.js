222200d6f584d8b879cdbc860418e332b36abf22
Stef Oostveen <soostveen@electic.nl>
2020-10-24 21:33:21 +0200

    conf update

--- src/index.js
+++ src/index.js
@@ -12,38 +12,21 @@ const imageList = [img1, img2, img3, img4]
 class Demo extends Component {
   constructor(props) {
     super(props)
-    this.state = {
-      image: null,
-      images: []
-    }
+    this.state = {image: null}
   }
 
-  onPickImage(image) {
+  onPick(image) {
     this.setState({image})
-  }
-
-  onPickImages(images) {
-    this.setState({images})
   }
 
   render() {
     return (
       <div>
-        <h1>React Image Picker</h1>
-        <h3>Single Select</h3>
         <ImagePicker 
           images={imageList.map((image, i) => ({src: image, value: i}))}
-          onPick={this.onPickImage.bind(this)}
+          onPick={this.onPick.bind(this)}
         />
-        <textarea rows="4" cols="100" value={this.state.image && JSON.stringify(this.state.image)} disabled/>
-        
-        <h3>Multiple Select</h3>
-        <ImagePicker 
-          images={imageList.map((image, i) => ({src: image, value: i}))}
-          onPick={this.onPickImages.bind(this)}
-          multiple
-        />
-        <textarea rows="4" cols="100" value={this.state.images && JSON.stringify(this.state.images)} disabled/>
+        <button type="button" onClick={() => console.log(this.state.image)}>OK</button>
       </div>
     )
   }
